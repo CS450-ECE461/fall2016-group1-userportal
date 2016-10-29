a7cb582e1ca74c1aa2163880df0dd997d9e84616
cahuizar <hire@carloshuizar.com>
2016-10-29 11:18:22 -0400

    Updated registration form and added css

--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -7,58 +7,59 @@ import { StateService } from 'ui-router-ng2';
   selector: 'resgister',
   template: `
 
-    <p>Register<p>
-    <form>
-      <div class="form-group">
-        <label for="id">Username*</label>
-        <div class ="row">
-          <div class="col-xs-12 col-md-3">
-            <input type="text" class="form-control" id="id" required>
-          </div>
-          <div class="col-md-9"></div>
+    <h2>Register</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-4">
+          <form>
+            <div class="form-group">
+              <label for="id">Username*</label>
+              <div class ="row">
+                <div class="col-xs-12 col-md-7">
+                  <input type="text" class="form-control" id="id" required>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="firstName">First Name*</label>
+              <div class ="row">
+                <div class="col-xs-12 col-md-7">
+                  <input type="text" class="form-control" id="firstName" required>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="middleName">Middle Name</label>
+              <div class ="row">
+                <div class="col-xs-12 col-md-7">
+                  <input type="text" class="form-control" id="middleName">
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="lastName">Last Name*</label>
+              <div class ="row">
+                <div class="col-xs-12 col-md-7">
+                  <input type="text" class="form-control" id="lastName" required>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="emailAddress">Email*</label>
+              <div class ="row">
+                <div class="col-xs-12 col-md-7">
+                  <input type="text" class="form-control" id="emailAddress" required>
+                </div>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+          </form>
         </div>
-      </div>
-      <div class="form-group">
-        <label for="firstName">First  Name*</label>
-        <div class ="row">
-          <div class=" col-xs-12 col-md-3">
-            <input type="text" class="form-control" id="firstName" required>
-          </div>
-          <div class="col-md-9"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="middleName">Middle Name</label>
-        <div class ="row">
-          <div class="col-xs-12 col-md-3">
-            <input type="text" class="form-control" id="middleName">
-          </div>
-          <div class="col-md-9"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="lastName">Last Name*</label>
-        <div class ="row">
-          <div class=" col-xs-12 col-md-3">
-            <input type="text" class="form-control" id="lastName" required>
-          </div>
-          <div class="col-md-9"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="emailAddress">Email*</label>
-        <div class ="row">
-          <div class="col-xs-12 col-md-3">
-            <input type="text" class="form-control" id="emailAddress" required>
-          </div>
-          <div class="col-md-9"></div>
-        </div>
-      </div>
-      <button type="submit" class="btn btn-primary">Register</button>
-    </form>
+    </div>
     <br/>
     <a uiSref="main">Home</a>
     <a uiSref="login">Login</a>
+  </div>
   `
 })
 export class RegisterComponent {}
