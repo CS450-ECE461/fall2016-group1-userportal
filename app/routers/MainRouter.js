d852d56f5f5b40c8044175a405f150383f90e8b2
cahuizar <hire@carloshuizar.com>
2016-11-09 07:40:09 -0500

    Removed Home view #15

--- app/routers/MainRouter.js
+++ app/routers/MainRouter.js
@@ -1,9 +1,6 @@
 var passport = require ('passport')
 ;
 module.exports = exports = {
-  '/home' : {
-    get  : { view   : 'home.pug' }
-  },
   '/login': {
     get  : { view : 'login.pug'},
     post : {action: 'LoginController@completeLogin'},
