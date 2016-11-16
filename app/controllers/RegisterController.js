b57a957bb30b513894467477e944dc1fd42ae4df
Mohammad <MohammadHamad@Mohammads-MacBook-Pro.local>
2016-11-16 01:14:50 -0500

    working on validation issue #14
    -fixing typo
    -adding comments

--- app/controllers/RegisterController.js
+++ app/controllers/RegisterController.js
@@ -19,13 +19,15 @@ RegisterController.prototype.completeSignUp = function () {
        "password" : req.body.password,
         "handle" : req.body.username,
     };
-    regExp = /^[a-zA-Z]+$/;  
+    var regExp = /^[a-zA-Z]+$/;  
     request
         .post('localhost:5000/api/v1/users')
         .send({ user: user})
         .end(function (err, resp, user){
           if(err || req.body.firstName || req.body.middleName || req.body.lastName){
-              
+            //search checks the user input if it contains letters only
+            //returns -1 if it contains anything other than letters
+            // throws error notifying user
             if(req.body.firstName.search(regExp) == -1 || req.body.middleName.search(regExp) == -1 || req.body.lastName.search(regExp) == -1){
                 res.render('register.pug', {error_message: 'First/middle/last name should contain letters only'});
             } 
