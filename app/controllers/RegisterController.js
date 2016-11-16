a728bb144ca6afa1efe2875ee8ff77bcfde2e13b
Mohammad <MohammadHamad@Mohammads-MacBook-Pro.local>
2016-11-16 00:43:54 -0500

    working validation issues #14
    validation for:
    fname/lastname/middlename = complete
    
    password: todo
    confirm pass: todo

--- app/controllers/RegisterController.js
+++ app/controllers/RegisterController.js
@@ -8,17 +8,9 @@ function RegisterController () {
 
 blueprint.controller (RegisterController);
 
+
 RegisterController.prototype.completeSignUp = function () {
   return function (req, res) {
-  // Error: User is not initialized
-  // var user = new User ({
-  //   "username" : req.body.username,
-  //   "firstName" : req.body.firstName,
-  //   "middleName" : req.body.middleName,
-  //   "lastName" : req.body.lastName,
-  //   "emailAddress" :  req.body.emailAddress,
-  //   "password" : req.body.password
-  // });
      var user = {
        "firstName" : req.body.firstName,
        "middleName" : req.body.middleName,
@@ -27,20 +19,29 @@ RegisterController.prototype.completeSignUp = function () {
        "password" : req.body.password,
         "handle" : req.body.username,
     };
-    console.log('Username: '+user.username);
-    console.log('First Name: '+user.firstName);
-    console.log('Middle Name: '+user.middleName);
-    console.log('Last Name: '+user.lastName);
-    console.log('Email: '+user.emailAddress);
-    console.log('Password: '+user.password);
+    regExp = /^[a-zA-Z]+$/;  
     request
         .post('localhost:5000/api/v1/users')
-        .send(user)
-        .end(function (err, resp){
-          if(err){
-            console.log(err);
-          }
-          else{
+        .send({ user: user})
+        .end(function (err, resp, user){
+          if(err || req.body.firstName || req.body.middleName || req.body.lastName){
+              
+            if(req.body.firstName.search(regExp) == -1 || req.body.middleName.search(regExp) == -1 || req.body.lastName.search(regExp) == -1){
+                res.render('register.pug', {error_message: 'First/middle/last name should contain letters only'});
+            } 
+            if(err == null){
+              res.render('register.pug', {error_message: 'Username and or email have been used already.'});
+            }
+            else if(err.status == '422'){
+              res.render('register.pug', {error_message: 'Username and or email have been used already.'});
+            }
+            else if(err.status == '409') {
+              res.render('register.pug', {error_message: 'Email has already been used.'});
+            }
+            else{
+              res.render('register.pug', {error_message: 'Something went wrong, try again.'});
+            }
+          }else{
             return  res.redirect('/login');
           }
         });
