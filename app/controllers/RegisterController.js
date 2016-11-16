eb191aa91d363150d1d1eeb42a9145fb9427c264
Carlos <hire@carloshuizar.com>
2016-11-16 17:33:57 -0500

    Added Mohammad's validation

--- app/controllers/RegisterController.js
+++ app/controllers/RegisterController.js
@@ -19,23 +19,28 @@ RegisterController.prototype.completeSignUp = function () {
        "password" : req.body.password,
         "handle" : req.body.username,
     };
+    var regExp = /^[a-zA-Z]+$/;
     request
         .post('localhost:5000/api/v1/users')
         .send({ user: user})
         .end(function (err, resp){
-          if(err){
-            if(err.status == '422'){
-              res.render('register.pug', {error_message: 'Username and or email have been used already.'});
-            }
-            else if(err.status == '409') {
-              res.render('register.pug', {error_message: 'Email has already been used.'});
-            }
-            else{
-              res.render('register.pug', {error_message: 'Something went wrong, try again.'});
-            }
-          }else{
-            return  res.redirect('/login');
-          }
+            if(req.body.firstName.search(regExp) == -1 || req.body.middleName.search(regExp) == -1 || req.body.lastName.search(regExp) == -1){
+                res.render('register.pug', {error_message: 'First/middle/last name should contain letters only'});
+              }
+              if(err){
+                if(err.status == '422'){
+                   res.render('register.pug', {error_message: 'Username and or email have been used already.'});
+                }
+                else if(err.status == '409') {
+                  res.render('register.pug', {error_message: 'Email has already been used.'});
+                }
+                else{
+                  res.render('register.pug', {error_message: 'Something went wrong, try again.'});
+                }
+              }else{
+                return  res.redirect('/login');
+              }
+
         });
   };
 
