9be564994eb462e032fb0fcd9d862968dcd0aeb0
Carlos <hire@carloshuizar.com>
2016-11-21 13:52:17 -0500

    Controller uses the passport file

--- app/routers/MainRouter.js
+++ app/routers/MainRouter.js
@@ -3,7 +3,9 @@ var passport = require ('passport')
 module.exports = exports = {
   '/login': {
     get  : { view : 'login.pug'},
-    post : {action: 'LoginController@completeLogin'},
+    post : {
+      before: [passport.authenticate('local', {failureRedirect:'/login'})],
+      action: 'LoginController@completeLogin'},
   },
   '/register': {
     get  : { view : 'register.pug'},
