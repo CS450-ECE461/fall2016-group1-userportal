10c1c2455997cfc328c5b2352c1500aeb7d86fcc
Carlos Huizar <Carlos Huizar>
2016-12-01 21:07:06 -0500

    token deserialized

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -2,6 +2,7 @@
 
 var blueprint = require ('@onehilltech/blueprint')
   ;
+var token;
 
 module.exports = UserController;
 
@@ -18,11 +19,37 @@ UserController.prototype.signout = function () {
 
     };
 };
+function getName(){
+  var newUser = {
+    "username" : username,
+    "password" : password
+  };
+  console.log("Username: "+newUser.username);
+  console.log("Password: "+newUser.password);
+  console.log("New User: "+ newUser )
+  request
+      .post('localhost:5000/api/v1/auth/jwt')
+      .type("json")
+      .set("Accept", "application/json")
+      .send(newUser)
+      .end(function (error, resp){
+        if(error){
+          if(error.status == '422')
+            console.log(error);
+        }
+        else{
+          var token = resp.body.jwt;
+          console.log(token);
+          return done (null, token);
+        }
+        return done(null, false);
+      });
+}
 
 UserController.prototype.showMe = function () {
   return function (req, res) {
-    var jwt = req.token;
-    console.log("Hello my token is: "+ req.token);
-    res.render ('dashboard.pug', {users: req.token});
+    token = req.user
+
+    res.render ('dashboard.pug', {welcome:"Welcome "+name });
   }
 };
